1d0c119f55f2ba807bc474f5dcea47db9f6ea015
eric capiz <ericcapiz@gmail.com>
2024-04-02 11:04:35 -0500

    feat: can sort repos

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -11,6 +11,7 @@ const Home = () => {
   const [repos, setRepos] = useState([]);
   const [loading, setLoading] = useState(false);
   const [sortType, setSortType] = useState("forks");
+  console.log("repos", repos);
 
   const getUserProfileAndRepos = useCallback(async (username = "ericcapiz") => {
     setLoading(true);
@@ -48,10 +49,22 @@ const Home = () => {
     setLoading(false);
   };
 
+  const onSort = (sortType) => {
+    if (sortType === "recent") {
+      repos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+    } else if (sortType === "stars") {
+      repos.sort((a, b) => b.stargazers_count - a.stargazers_count);
+    } else if (sortType === "forks") {
+      repos.sort((a, b) => b.forks_count - a.forks_count);
+    }
+    setSortType(sortType);
+    setRepos([...repos]);
+  };
+
   return (
     <div className="m-4">
       <Search onSearch={onSearch} />
-      <SortRepos />
+      {repos.length > 0 && <SortRepos onSort={onSort} sortType={sortType} />}
       <div className="flex gap-4 flex-col lg:flex-row justify-center items-start">
         {userProfile && !loading && <ProfileInfo userProfile={userProfile} />}
         {!loading && <Repos repos={repos} />}
